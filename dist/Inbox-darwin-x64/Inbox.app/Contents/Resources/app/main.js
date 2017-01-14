88458cfe60f92bbd47315fce40b5e065a0fd41f3
Matt Pilott <matt@creativelittledots.co.uk>
2017-01-14 18:37:13 +0000

    Initial Push
    Signed-off-by: Matt Pilott <matt@creativelittledots.co.uk>

---
+++ dist/Inbox-darwin-x64/Inbox.app/Contents/Resources/app/main.js
@@ -0,0 +1,75 @@
+/*----------------------------------------------*\
+	#SETUP WINDOW
+\*----------------------------------------------*/
+
+const {app, Menu, BrowserWindow, shell} = require('electron')
+
+let win
+
+function createWindow() {
+    
+    win = new BrowserWindow({
+        
+        width : 1280,
+        height : 1024,
+        titleBarStyle : 'hidden-inset',
+        title : 'Inbox',
+        transparent : true,
+        vibrancy : 'light'
+    })
+
+    win.loadURL(`file://${__dirname}/index.html`)
+    
+    //win.openDevTools();
+    
+    win.on('closed', function () {
+        
+        win = null
+    })
+    
+    /*  COPY & PASTE  */
+
+    let template = [{
+        label: "Application",
+        submenu: [
+            { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]
+    }, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]
+    }]
+    
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+    
+    /*  END  */
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+    
+    if (process.platform !== 'darwin') {
+        
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    
+    if (win === null) {
+        
+        createWindow()
+    }
+})
+
+
---
+++ scripts/render.js
@@ -0,0 +1,130 @@
+const fs = require('fs')
+
+let css;
+
+fs.readFile(__dirname + '/styles/inbox.css', "utf-8", function(error, data) {
+    
+    if(!error){
+        
+        css = data.replace(/\s{2,10}/g, ' ').trim();
+    }
+});
+
+onload = () => {
+    
+    const webview = document.getElementById('webview');
+    
+    webview.addEventListener("dom-ready", function(){
+        
+        webview.insertCSS(css)
+        
+        //webview.openDevTools()
+    });
+    
+    const wait = (function(){
+        
+        var timer = 0;
+        
+        return function(callback, ms){
+            
+            clearTimeout (timer);
+            
+            timer = setTimeout(callback, ms);
+        };
+    })();
+    
+    let loaded = false;
+    
+    const loadstart = () => {
+        
+        document.querySelector('.status').classList.remove('status--bad', 'status--good', 'status--confused');
+        
+        if ( !loaded ) {
+            
+            setTimeout(function(){
+        
+                document.querySelector('.intro').classList.remove('intro--hide');
+            }, 750);
+        }
+    };
+    
+    const loadstop = () => {
+        
+        loaded = true;
+        
+        wait(function(){
+            
+            document.querySelector('.intro').classList.add('intro--hide');
+            
+            setTimeout(function(){
+                
+                document.querySelector('.webview').classList.add('webview--show');
+            }, 250);
+        }, 250);
+    };
+    
+    const loadFail = () => {
+        
+        document.querySelector('.status').classList.add('status--bad');
+        
+        document.querySelector('.status__button').innerHTML = 'Reload';
+        
+        document.querySelector('.status__message').innerHTML = 'Oh no! The Myth of the Internet continues';
+    };
+
+    const handleRedirect = (e) => {
+
+        if ( e.url.includes('?authuser=') ) {
+
+            e.preventDefault();
+            
+            webview.loadURL(e.url);
+        }
+        
+        else if( ! e.url.includes(webview.getURL()) ) {
+
+            e.preventDefault();
+
+            shell.openExternal(e.url);
+        }
+    };
+
+    const switcher = (e) => {
+
+        if ( e.ctrlKey && (e.keyCode === 49 || e.keyCode === 50) ) {
+
+            let user = e.keyCode === 49 ? '0' : '1';
+
+            loaded = false;
+
+            document.querySelector('.webview').classList.remove('webview--show');
+
+            document.querySelector('.intro').classList.add('intro--hide');
+
+            webview.loadURL('https://inbox.google.com/u/0/?authuser=' + user);
+        }
+    };
+    
+    const button = () => {
+        
+        if ( document.querySelector('.status').classList.contains('status--bad') ) {
+        
+            webview.reload();
+        }
+    };
+    
+    webview.addEventListener('did-start-loading', loadstart);
+    
+    webview.addEventListener('did-stop-loading', loadstop);
+    
+    webview.addEventListener('did-fail-load', loadFail);
+
+    webview.addEventListener('will-navigate', handleRedirect);
+
+    webview.addEventListener('new-window', handleRedirect);
+
+    webview.addEventListener('keyup', switcher);
+
+    document.querySelector('.js-button').addEventListener('click', button);
+
+};
