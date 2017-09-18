58b0bbdc50b629921657563688b1c5ed007d61ab
Matt Pilott <matt@creativelittledots.co.uk>
2017-09-18 18:46:15 +0100

    Updated electron

--- app/index.js
+++ app/index.js
@@ -14,19 +14,19 @@ const fs = require('fs');
 const menu = require('./menu');
 
 function createWindow() {
-    
+
     const initPath = path.join(app.getPath('userData'), 'init.json');
-    
+
     let data = {};
-    
+
     try {
-        
+
         data = JSON.parse(fs.readFileSync(initPath, 'utf8'));
     }
     catch(e) {}
-    
+
     win = new BrowserWindow({
-        
+
         width : data.bounds ? data.bounds.width : 1280,
         height : data.bounds ? data.bounds.height : 1024,
         x : data.pos ? data.pos[0] : null,
@@ -38,47 +38,47 @@ function createWindow() {
     });
 
     win.loadURL(`file://${__dirname}/../web/index.html`);
-    
+
     //win.openDevTools();
-    
+
     function saveWin() {
-        
+
         data.bounds = win.getBounds();
         data.pos = win.getPosition();
-        
+
         fs.writeFileSync(initPath, JSON.stringify(data));
     }
-    
+
     win.on('resize', saveWin);
-    
+
     win.on('move', saveWin);
-    
+
     win.on('closed', function() {
-        
+
         win = null;
     });
-    
+
     first = false;
-    
+
     menu();
-    
+
     /*  END  */
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', function() {
-    
+
     if (process.platform !== 'darwin') {
-        
+
         app.quit();
     }
 });
 
 app.on('activate', function() {
-    
+
     if (win === null) {
-        
+
         createWindow();
     }
 });
